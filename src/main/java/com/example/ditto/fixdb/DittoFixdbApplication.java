b67476c1c1c354e55097d3a760f00b0dabdc1923
tom <8225602+elonderin@users.noreply.github.com>
2022-07-05 15:35:18 +0200

    initializr generated content as-is

---
+++ src/main/java/com/example/ditto/fixdb/DittoFixdbApplication.java
@@ -0,0 +1,13 @@
+package com.example.ditto.fixdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DittoFixdbApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DittoFixdbApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/ditto/fixdb/DittoFixdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ditto.fixdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DittoFixdbApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
